6804bdfce50c1744017087ff3c4b228114aff74e
Chejohrpp <sergioroherp@gmail.com>
2021-03-27 13:33:35 -0600

    Se mejoro la apariencia visual del tema violet

--- WebServiceP1C1/src/main/java/Controladores/MostrarFormulario.java
+++ WebServiceP1C1/src/main/java/Controladores/MostrarFormulario.java
@@ -89,6 +89,7 @@ public class MostrarFormulario extends HttpServlet {
             out.println("</form>");
             out.println("</div>");
             out.println("</div>");
+            out.println("<script src=\"scripts/creacionFormularios.js\"></script>");
             out.println("</body>");
             out.println("</html>");
         }            
@@ -123,16 +124,21 @@ public class MostrarFormulario extends HttpServlet {
             }
             case "RADIO":{
                 out.println("<label>"+textoVisible+"</label>");
+                out.println("<div class=\"box\">");  
                 String[] opciones = opciones(componente.getOpciones());
                 for (String opcione : opciones) {
                     out.println(" <input type=\"radio\" name=\""+componente.getNombreCampo()+"\"  value=\""+opcione+"\" "+requerido+" />"); 
                     out.println(" <label id=\"opciones\">"+opcione+"</label><br>"); 
-                }              
+                }
+                out.println("</div>");
                 break;
             }
             case "FICHERO":{
                 out.println("<label>"+textoVisible+"</label>");           
-                out.println("  <input type=\"file\" name=\""+componente.getNombreCampo()+"\"   "+requerido+"/>");                
+                out.println("<div class=\"box\">");           
+                out.println("<input type=\"file\" id=\"file-1\" name=\""+componente.getNombreCampo()+"\" class=\"inputfile inputfile-1\"  data-multiple-caption=\"{count} files selected\" multiple   "+requerido+"/>");                
+                out.println("<label for=\"file-1\" id=\"labelFile\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"17\" viewBox=\"0 0 20 17\"><path d=\"M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z\"/></svg> <span>Escoge un archivo&hellip;</span></label>");                
+                out.println("</div>");
                 break;
             }
             case "IMAGEN":{
